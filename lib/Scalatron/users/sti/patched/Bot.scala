f8b2533e9fdaac3187568f6ccb116b759cf6eeba
stihenri <stig.henriksen@finn.no>
2013-10-27 21:44:38 +0100

    Cleanup.

--- lib/Scalatron/users/sti/patched/Bot.scala
+++
@@ -1,7 +0,0 @@
-package scalatron.botwar.botPlugin.sti
-class ControlFunctionFactory {
-def create = new ControlFunction().respond _
-}
-class ControlFunction {
-  def respond(input: String) = "Status(text=Hello Worldscck" + mybot.Utils.stupid() + ")"
-}
