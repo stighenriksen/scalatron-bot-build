899e59a38278ff3b25efb861faca14f7d00cb350
stihenri <stig.henriksen@finn.no>
2013-10-30 20:38:13 +0100

    Make it compile...

--- src/main/scala/Bot.scala
+++ src/main/scala/Bot.scala
@@ -5,12 +5,6 @@ def create = new ControlFunction().respond _
 class ControlFunction {
   val a = 2
   def respond(input: String) = {
-    CommandParser(input) match {
-      case React(generation: Int, tie: Int, view: String, energy: String, name: String, master: Seq[(Int, Int)]) => {
-        val r = Math.random
-        Move(direction = Heading(1, 2)).toString
-    }
-      case _ => ""
+    "Status(text=Hello Scalatron!)"
   }
-}
 }
